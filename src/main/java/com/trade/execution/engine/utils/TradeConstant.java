4f67bdefe28754a0c9af7ce92ac3156326528b06
Virendra Singh <virendra.bisht2006@gmail.com>
2017-10-24 08:29:14 +0530

    refactoring of classes

--- src/main/java/com/trade/execution/engine/utils/TradeConstant.java
+++ src/main/java/com/trade/execution/engine/utils/TradeConstant.java
@@ -3,9 +3,9 @@ package com.trade.execution.engine.utils;
 
 public class TradeConstant {
 
-    protected static final String DATE_FORMAT = "dd MMM yyyy";
-    protected static final String CURRENCY_AED = "AED";
-    protected static final String CURRENCY_SAR = "SAR";
+    public static final String DATE_FORMAT = "dd MMM yyyy";
+    public static final String CURRENCY_AED = "AED";
+    public static final String CURRENCY_SAR = "SAR";
 
     private TradeConstant() {
 
